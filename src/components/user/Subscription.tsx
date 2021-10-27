c20cd3390059f2e2f3d0c9e228195d7d7cdac633
Josefine Löfgren <josefinelofgren@Josefines-MacBook-Pro.local>
2021-10-27 19:18:26 +0200

    add components

---
+++ src/components/user/Subscription.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// inloggad användare kan se sin prenumeration
+
+function Subscription() {
+  return (
+    <div className='subscription'>
+     
+    </div>
+  );
+}
+
+export default Subscription;
