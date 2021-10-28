ac9fbf42c135a2ac62aa68153c46fc2178e73c09
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-10-28 09:55:52 +0200

    Creating component HowItWorks

---
+++ src/components/HowItWorks.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+
+function HowItWorks() {
+  return (
+    <div className='how-it-works'>
+      <Container fluid>
+          <div className='how-it-works-content'>
+            <h2>Hur funkar DISKRID?</h2>
+
+              {/* <h1 className='title'>Här ska det vara en slogan</h1>
+              <Button className='btn-white'>Kom igång</Button> */}
+          </div>
+      </Container>
+    </div>
+  );
+}
+
+export default HowItWorks;
