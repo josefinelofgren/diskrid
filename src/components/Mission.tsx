ec357d6719642b4fa1b4047c2ac04f0956870cec
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-10-28 12:50:38 +0200

    Added split screen layout with text and img

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 function Mission() {
   return (
     <div className='mission'>
       <Container fluid>
           <div className='mission-content'>
-              <section className="mission-left">left</section>
-              <section className="mission-right">right</section>
+              <section className="mission-left">
+                <p className="mission-title">Håll det rent, <br />med gott samvete</p>
+                <p className="mission-text">Vi på Diskrid m dolor sit amet, consectetur adipiscing elit. Integer id felis scelerisque, hendrerit ex faucibus</p>
+              </section>
+              <section className="mission-right"></section>
           </div>
       </Container>
     </div>
